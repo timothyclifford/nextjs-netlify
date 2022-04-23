160910a304ba820d60047afc3d4e5d3170ee6265
Timothy Clifford <tim.cliford@gmail.com>
2022-04-23 18:23:10 +1000

    add nextjs + netlify cms

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: (cfg) => {
+      cfg.module.rules.push(
+          {
+              test: /\.md$/,
+              loader: 'frontmatter-markdown-loader',
+              options: { mode: ['react-component'] }
+          }
+      )
+      return cfg;
+  }
+}
